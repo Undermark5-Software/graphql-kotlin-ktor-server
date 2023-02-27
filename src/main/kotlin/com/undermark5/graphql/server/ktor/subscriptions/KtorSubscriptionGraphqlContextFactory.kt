f84d4262b9f994177ae49d0e3e0142e94d354081
undermark5 <undermark5@undermark5.com>
2023-02-26 18:13:42 -0600

    update copyright notice

--- src/main/kotlin/com/undermark5/graphql/server/ktor/subscriptions/KtorSubscriptionGraphqlContextFactory.kt
+++ src/main/kotlin/com/undermark5/graphql/server/ktor/subscriptions/KtorSubscriptionGraphqlContextFactory.kt
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Modified from the original to implement with Ktor - undermark5 2023
  */
 
 package com.undermark5.graphql.server.ktor.subscriptions
